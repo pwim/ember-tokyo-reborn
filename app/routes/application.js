00ac4d6b73ff38a8ac2d3b051d4264deb88a7105
dopin <dopinninja@gmail.com>
2017-01-26 15:53:06 +0900

    IndexError

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,12 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    return new Ember.RSVP.Promise((_, reject) => {
-      reject('Error!');
-    });
-  },
-
   renderTemplate() {
     this.render('application');
   },
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return new Ember.RSVP.Promise((_, reject) => {
+      reject('Error!');
+    });
+  },
   renderTemplate() {
     this.render('index', {
       into: 'application',
