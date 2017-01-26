d655acf55509a5df2f97cecb202ffae9c5bbd05f
dopin <dopinninja@gmail.com>
2017-01-26 15:40:03 +0900

    application-error route

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return new Ember.RSVP.Promise((_, reject) => {
+      reject('Error!');
+    });
+  },
+
   renderTemplate() {
     this.render('application');
   },
